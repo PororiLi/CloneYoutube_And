f61b715e954a580e70b0ff491aae70f8ac545a76
pororili <xiozioll@gmail.com>
2021-04-01 19:13:39 +0900

    create project

---
+++ app/src/main/java/com/porori/cloneyoutube/MainActivity.java
@@ -0,0 +1,14 @@
+package com.porori.cloneyoutube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
